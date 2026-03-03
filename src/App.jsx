471e747f40e34f5cbfab03137ce552d0d1d83f49
Art1ku <alihaniskenderbekov@gmail.com>
2026-03-03 12:54:04 +0600

    l

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Notes from "./pages/Notes";
+import NoteDetails from "./pages/NoteDetails";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <nav style={{ padding: "10px", background: "#f2f2f2" }}>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/notes">Notes</Link> |{" "}
+        <Link to="/about">About</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/notes" element={<Notes />} />
+        <Route path="/notes/:id" element={<NoteDetails />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
